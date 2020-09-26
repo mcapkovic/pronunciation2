f62b1a0a5c002d9e091320558d21ae7170f1939c
Matej Capkovic <mcapkovic@gmail.com>
2020-09-26 17:54:48 +0200

    Change default url

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,9 @@
-export const ITEM_BOOKMARKS = 'bookmarks';
+export const ITEM_BOOKMARKS = "bookmarks";
 
-export const URL_BOOKMARKS = 'bkm';
-export const URL_VIDEO = 'sv';
+export const URL_BOOKMARKS = "bkm";
+export const URL_VIDEO = "sv";
 
-export const USER_FLAG = 'welcome';
+export const USER_FLAG = "welcome";
 
+export const DEFAULT_VIDEO_URL = 'https://youtu.be/IERJyt2qKh8';
+export const DEFAULT_LESSON = `${URL_VIDEO}=${DEFAULT_VIDEO_URL}&${URL_BOOKMARKS}=`;
